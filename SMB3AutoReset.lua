53f7948af2a9d632991858907d33c559c67b7550
tryhardsnipehrd <57113411+tryhardsnipehrd@users.noreply.github.com>
2021-01-03 11:33:04 -0700

    Update SMB3AutoReset.lua
    Changed the 2 ifs to a singe one with an or

--- SMB3AutoReset.lua
+++ SMB3AutoReset.lua
@@ -1,8 +1,5 @@
 while (true) do
-    if (memory.readbyte(0x00F1)==1) then
-        emu.poweron();
-    end;
-    if (memory.readbyte(0x00F1)==2) then
+    if (memory.readbyte(0x00F1)==1 or memory.readbyte(0x00F1)==2) then
         emu.poweron();
     end;
     emu.frameadvance();
